957a4072a5b258caaa76e95d1fa2504383dfeb05
Nahuel <nahuel.profesor@gmail.com>
2022-09-12 12:11:53 -0300

    avanzando con las funciones de agregarCurso y leerDatosCurso

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// Variables y selectores
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+// Funciones
+const cargarEventListeners = () => {
+    // Cuando agregas un curso presionando "Agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+
+
+}
+
+const agregarCurso = (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae la información del curso
+const leerDatosCurso = (curso) => {
+    console.log(curso);
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
+}
+
+
+
+cargarEventListeners();
